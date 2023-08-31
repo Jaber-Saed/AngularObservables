3add3c196f63786485d9ded7faa3a9d80c50e254
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-31 11:57:34 +0300

    Up date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
   title = 'AngularObservables';
+
+  myObservable = new Observable((observer) => {
+    
+    console.log('Observable Starts');
+
+    setTimeout(() => { observer.next("1") }, 1000)
+    setTimeout(() => { observer.next("2") }, 2000)
+    setTimeout(() => { observer.next("3") }, 3000)
+    setTimeout(() => { observer.next("4") }, 4000)
+    setTimeout(() => { observer.next("5") }, 5000)
+
+  })
+
+
+  ngOnInit(): void { }
+
+  ObservablesRun() {
+    this.myObservable.subscribe((val) => {
+      console.log(val);
+    })
+
+  }
 }
