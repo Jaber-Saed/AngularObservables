39ee1e1b758fd07eadf1100d062211579c1d8d22
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-31 12:06:31 +0300

    try Observables error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,16 @@ export class AppComponent implements OnInit {
   constructor() { }
 
   title = 'AngularObservables';
-
+  errorEnsert = new Error(`Something went wrong!`)
   myObservable = new Observable((observer) => {
     
     console.log('Observable Starts');
 
+
     setTimeout(() => { observer.next("1") }, 1000)
     setTimeout(() => { observer.next("2") }, 2000)
     setTimeout(() => { observer.next("3") }, 3000)
+    setTimeout(() => { observer.error(this.errorEnsert) }, 3200)
     setTimeout(() => { observer.next("4") }, 4000)
     setTimeout(() => { observer.next("5") }, 5000)
 
